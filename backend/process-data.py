d1c83c47ef72bbabe13734cc6e846a3359bf1c70
Edison Zhang <edisonzhang@ucsb.edu>
2024-01-13 13:42:56 -0800

    changed similarity search architecture + added difficulty data

--- backend/process-data.py
+++ backend/process-data.py
@@ -5,6 +5,9 @@ import requests
 from langchain_openai import OpenAIEmbeddings
 import os
 from dotenv import load_dotenv
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import Pinecone
 
 # load .env file and retrieve API key
 load_dotenv()
@@ -20,27 +23,100 @@ index_name = "gaucho-genie"
 # Create a new Pinecone index or connect to an existing one
 index = pinecone.Index(index_name)
 
+# Variables for the loop
+quarters = ["20241"]
+file = open('subject_codes.csv', 'r')
+subject_codes = file.read().split(', ')
+
+# limit string length to reduce metadata size
+def limit_string_length(input_string, max_length):
+    if len(input_string) > max_length:
+        return input_string[:max_length]
+    return input_string
+
+# convert quarter code to string for better readability
+def quarter_code_to_string(quarter_code):
+    year = str(quarter_code[:4])
+    quarter_season = str(quarter_code[4])
+
+    if quarter_season == "1":
+        quarter_season = "Winter"
+    elif quarter_season == "2":
+        quarter_season = "Spring"
+    elif quarter_season == "3":
+        quarter_season = "Summer"
+    elif quarter_season == "4":
+        quarter_season = "Fall"
+
+    return f"{year} {quarter_season}"
+
+# load course difficulty data from csv and make dictionary
+file = open('course_difficulty_ratings.csv', 'r')
+course_difficulty = {}
+
+for line in file:
+    line = line.split(',')
+    key = " ".join(line[0].split())
+    value = line[1].strip()
+    course_difficulty[key] = value
+
 # Load the JSON data
-with open("20241-classes/ANTH.json", 'r') as file:
-    data = json.load(file)
+for quarter in quarters:
+    for subject in subject_codes:
+        with open(f"{quarter}-classes/{subject}.json", 'r') as file:
+            data = json.load(file)
 
-classes = data.get("classes", [])
+        classes = data.get("classes", [])
 
-for class_info in classes:
-    print("BEFORE")
+        for class_info in classes:
+            quarter_string = quarter_code_to_string(quarter)
+            courseId = " ".join(class_info['courseId'].split())
+            title = str(class_info['title'])
+            description = str(class_info['description'])
+            college = str(class_info['college'])
+            subjectArea = str(class_info['subjectArea'].strip())
+            unitsFixed = str(class_info['unitsFixed'])
+            deptCode = str(class_info['deptCode'].strip())
+            generalEducation = str(class_info['generalEducation'])
 
-    # dump json into a string
-    class_string = json.dumps(class_info, indent=4)
+            # dump json into a string
+            class_string = f"""{courseId} {quarter_string} Overview:
+            The course is titled {title}.
+            It is offered by the {college} college and is in the {subjectArea} subject area.
+            It is worth {unitsFixed} units and is in the {deptCode} department.
+            It offers the following general education credits: {generalEducation}.
+            Please refer to the key when interpreting the general education credits.
+            The course description is as follows: {description}
+            The course difficulty is {course_difficulty.get(courseId, "not found")}.
+            """
 
-    # get embedding for class_string
-    embedding = embeddings.embed_query(class_string)
+            # get embedding for class_string
+            embedding = embeddings.embed_query(class_string)
 
-    # Ensure embedding length matches Pinecone index's dimension
-    assert len(embedding) == 1536, f"Embedding length does not match Pinecone index dimension {len(embedding)}"
+            # Ensure embedding length matches Pinecone index's dimension
+            assert len(embedding) == 1536, f"Embedding length does not match Pinecone index dimension {len(embedding)}"
 
-    # Prepare the data for Pinecone (id, vector pair)
-    vector_id = class_info['courseId'].strip()  # Assuming courseId is unique and used as ID
-    vector_data = (vector_id, embedding)
+            # Prepare the data for Pinecone (id, vector pair)
+            vector_id = f"{' '.join(class_info['courseId'].split())} {quarter}"  # Assuming courseId is unique and used as ID
+            print(vector_id)
 
-    # Perform the upsert operation
-    index.upsert(vectors=[vector_data])
+            # Metadata def
+            metadata = {
+                "quarter": quarter,
+                "courseId": courseId,
+                "title": title,
+                "description": description,
+                "college": college,
+                "subjectArea": subjectArea,
+                "unitsFixed": unitsFixed,
+                "deptCode": deptCode,
+                "generalEducation": generalEducation,
+                "text" : class_string
+            }
+
+            # Perform the upsert operation
+            index.upsert(vectors=[{
+                "id" : vector_id,
+                "values" : embedding,
+                "metadata" : metadata
+            }])
--- backend/searchtest.py
+++ backend/searchtest.py
@@ -1,5 +1,11 @@
-from langchain_openai import ChatOpenAI
-from langchain_openai import OpenAIEmbeddings
+from langchain.vectorstores.pinecone import Pinecone
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
+)
+from langchain.schema import HumanMessage, SystemMessage
 from langchain.chains.question_answering import load_qa_chain
 from dotenv import load_dotenv
 import pinecone
@@ -10,26 +16,88 @@ load_dotenv()
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
+# initialize pinecone
+pinecone.init(
+    api_key=PINECONE_API_KEY,  # find at app.pinecone.io
+    environment="gcp-starter",  # next to api key in console
+)
+
+index_name = "gaucho-genie"
+
 embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
 
-# Pinecone setup
-pinecone.init(PINECONE_API_KEY, environment='gcp-starter')
-index_name = "gaucho-genie"
-
-# Create a new Pinecone index or connect to an existing one
 index = pinecone.Index(index_name)
 
-# Pinecone setup
-pinecone.init(PINECONE_API_KEY, environment='gcp-starter')
-index_name = "gaucho-genie"
+docsearch = Pinecone.from_existing_index(index_name, embeddings)
 
-# Create a new Pinecone index or connect to an existing one
-index = pinecone.Index(index_name)
+query = input()
 
-# Query the index
-query = "Return the CMPSC courses with the lowest numbers"
-xq = embeddings.embed_query(query)
-# print(xq)
-res = index.query([xq], top_k=5, include_metadata=True)
+result = docsearch.similarity_search(query)
+
+print(result)
+
+def prepare_data_for_llm(query, results):
+    if not results:
+        return f"Query: {query}\n\nNo results found."
+
+    response_prompt = f"Query: {query}\n\nResults:\n {results}"
+
+    return response_prompt
+
+response_prompt = prepare_data_for_llm(query, result)
+
+chat = ChatOpenAI(temperature=0, openai_api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo-0613")
+
+messages = [
+    SystemMessage(
+        content="""
+        You are a helpful course planning assistant.
+        You are given a query and a list containing important information formatted as such 'ID: {id} Info: {metadata}'.
+        You must return the most relevant courses to the query as well as provide a short description based on the knowledge you have.
+
+        If the information is a course here is helpful information on how it is formatted, use this to help you generate a better response:
+
+        Course ID's are formatted as the following:
+        SUBJECT_ID COURSE_NUMBER
+        For example : CMPSC 8 is the course ID for Intro to Computer Science.
+
+        Course ID's lower than 100 are lower division undergraduate courses and 1xx courses are upper division undergraduate courses.
+
+        General education codes are formatted as the following:
+        geCollege is the college the general education requirement is for (e.g L&S is the College of Letters and Science and ENGR is the College of Engineering)
+        geCode is the code for the general education requirement
+        geCode meanings:
+            A - English Reading and Composition
+            B - Foreign Language
+            C - Science, Mathematics, and Technology
+            D - Social Science
+            E - Culture and Thought
+            F - Arts
+            G - Literature
+            EUR - European Traditions
+            WRT - Writing
+            NWC - World Cultures
+            QNT - Quantitative Relationships
+            ETH - Ethnicity
+
+        The course difficulty is rated on a scale of 1-10 with 1 being the easiest and 10 being the hardest.
+
+        The course data is formatted as such:
+        {courseId} {quarter_string} Overview:
+        The course is titled {title}.
+        It is offered by the {college} college and is in the {subjectArea} subject area.
+        It is worth {unitsFixed} units and is in the {deptCode} department.
+        It offers the following general education credits: {generalEducation}.
+        Please refer to the key when interpreting the general education credits.
+        The course description is as follows: {description}
+        The course is {difficuty}.
+        """
+    ),
+    HumanMessage(
+        content=f"{query} \n\n Please answer by utilizing the information provided: \n\n {response_prompt}"
+    ),
+]
+
+res = chat(messages)
 
 print(res)
