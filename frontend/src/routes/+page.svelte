6bbfeb9376cca50eaf2aaedba4026d265e107edc
aden <adenjo19@gmail.com>
2024-01-13 21:48:53 -0800

    1st

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -8,64 +8,68 @@
 	import { goto } from '$app/navigation';
 </script>
 
-<Header />
-
-<div
-	class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14 bg-background dark:bg-background text-foreground min-h-screen max-h-screen h-screen overflow-hidden"
->
-	<h1
-		class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-[1200px] mx-auto w-full text-center font-semibold"
+<main class="h-screen bg-white dark:bg-black">
+	<div>
+		<Header />
+	</div>
+	
+	<div
+		class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14 text-foreground"
 	>
-		Course Selection<br />
-		Is <span class="line-through text-black-500"><span class="text-red-500">Hard</span></span>
-		<span class="text-green-500">Easy</span>
-	</h1>
-	<p class="text-xl sm:text-2xl md:text-3xl text-center max-w-[1000px] mx-auto w-full">
-		<span class="italic">Simplifying and Enhancing Your Course Selection Process with AI</span>
-	</p>
+		<h1
+			class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-[1200px] mx-auto w-full text-center font-semibold mt-16"
+		>
+			Course Selection<br />
+			Is <span class="line-through text-black-500"><span class="text-red-500">Hard</span></span>
+			<span class="text-green-500">Easy</span>
+		</h1>
+		<p class="text-xl sm:text-2xl md:text-3xl text-center max-w-[1000px] mx-auto w-full">
+			<span class="italic">Simplifying and Enhancing Your Course Selection Process with AI</span>
+		</p>
 
-	<button
-		on:click={() => goto('/chat')}
-		class="font-bold inline-flex items-center text-3xl px-4 py-2 dark:bg-yellow-500 text-gray rounded-md cursor-pointer dark:hover:bg-yellow-400 bg-blue-950 hover:bg-blue-950/90 text-white dark:text-black"
-		>Let the Genie Know Your Wishes</button
-	>
+		<button
+			on:click={() => goto('/chat')}
+			class="font-bold inline-flex items-center text-3xl px-8 py-6 dark:bg-yellow-500 text-gray rounded-md cursor-pointer dark:hover:bg-yellow-400 bg-blue-950 hover:bg-blue-950/90 text-white dark:text-black"
+			>Let The Genie Know Your Wishes</button
+		>
 
-	<main class="container mx-auto p-4 text-foreground">
-		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-			<div
-				class="bg-blue-950 dark:bg-yellow-500 p-6 rounded-md shadow-lg dark:text-black text-white"
-			>
-				<h2 class="text-lg font-semibold mb-2 text-center">Estimate Course Difficulty</h2>
-				<p class="text-sm italic text-center">
-					Analyzes past student grades to estimate course diffiulty.
-				</p>
+		<main class="container mx-auto p-4 text-foreground">
+			<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+				<div
+					class="bg-blue-950 dark:bg-yellow-500 p-6 rounded-md shadow-lg dark:text-black text-white"
+				>
+					<h2 class="text-lg font-semibold mb-2 text-center">Estimate Course Difficulty</h2>
+					<p class="text-sm italic text-center">
+						Analyzes past student grades to estimate course diffiulty.
+					</p>
+				</div>
+				<div
+					class="bg-blue-950 dark:bg-yellow-500 p-6 rounded-md shadow-lg dark:text-black text-white"
+				>
+					<h2 class="text-lg font-semibold mb-2 text-center">Personalized Recommendations</h2>
+					<p class="text-sm italic text-center">
+						Tailored course recommendations are generated based on the analysis and specific user input.
+					</p>
+				</div>
+
+				<div
+					class="bg-blue-950 dark:bg-yellow-500 p-6 rounded-md shadow-lg dark:text-black text-white"
+				>
+					<h2 class="text-lg font-semibold mb-2 text-center">Instant Response</h2>
+					<p class="text-sm italic text-center">
+						No delays or waiting times whatsoever. 
+						Our system ensures immediate replies, providing a seamless and efficient user experience.
+					</p>
+				</div>
 			</div>
-			<div
-				class="bg-blue-950 dark:bg-yellow-500 p-6 rounded-md shadow-lg dark:text-black text-white"
-			>
-				<h2 class="text-lg font-semibold mb-2 text-center">Personalized Recommendations</h2>
-				<p class="text-sm italic text-center">
-					Tailored course recommendations are generated based on the analysis and specific user input.
-				</p>
-			</div>
+		</main>
 
-			<div
-				class="bg-blue-950 dark:bg-yellow-500 p-6 rounded-md shadow-lg dark:text-black text-white"
-			>
-				<h2 class="text-lg font-semibold mb-2 text-center">Instant Response</h2>
-				<p class="text-sm italic text-center">
-					No delays or waiting times whatsoever. 
-					Our system ensures immediate replies, providing a seamless and efficient user experience.
-				</p>
-			</div>
-		</div>
-	</main>
-
-	<p class="text-s text-center">
-		*We are not affiliated with the University of California, Santa Barbara.<br />
-		<span class="italic text-gray-500">This is a disclaimer statement.</span>
-	</p>
-</div>
+		<p class="text-s text-center">
+			*We are not affiliated with the University of California, Santa Barbara.<br />
+			<span class="italic text-gray-500">This is a disclaimer statement.</span>
+		</p>
+	</div>
+</main>
 
 <style>
 	:global(body) {
--- frontend/src/routes/chat/+page.svelte
+++ frontend/src/routes/chat/+page.svelte
@@ -12,32 +12,33 @@
 		}
 	};
 </script>
-
-<div>
-	<Header />
-</div>
-
-<main class="bg-background dark:bg-background w-full mx-auto p-4 text-foreground">
-	<div class="h-[680px] bg-gray-500 rounded-md bg-transparent">
-		{#each messages as { sender, text } (text)}
-			<div class="{sender === 'user' ? 'text-right' : 'text-left'} mb-2">
-				<span
-					class="inline-block bg-blue-500 text-white p-2 rounded {sender === 'user' ? 'user-message': ''}">{text}</span
-				>
-			</div>
-		{/each}
+<main class="h-screen bg-white dark:bg-black">
+	<div>
+		<Header />
 	</div>
 
-	<div class="mt-2 flex items-center text-foreground bg-background dark:bg-background px-5">
-		<input
-			bind:value={userInput}
-			class="text-gray-900  w-full p-2 border rounded bg-transparent mr-2 bg-background dark:bg-background"
-			placeholder="Ask the Genie"
-			on:keydown={(e) => e.key === 'Enter' && sendMessage()}
-		/>
-		<Button on:click={sendMessage}>
-			<ArrowLeftFromLine />
-		</Button>
+	<div class="w-full mx-auto p-4 text-foreground">
+		<div class="h-[680px] bg-gray-500 rounded-md bg-transparent">
+			{#each messages as { sender, text } (text)}
+				<div class="{sender === 'user' ? 'text-right' : 'text-left'} mb-2">
+					<span
+						class="inline-block bg-blue-500 text-white p-2 rounded {sender === 'user' ? 'user-message': ''}">{text}</span
+					>
+				</div>
+			{/each}
+		</div>
+
+		<div class="mt-2 flex items-center text-foreground bg-transparent px-5">
+			<input
+				bind:value={userInput}
+				class="text-white-900  w-full p-2 border border-gray dark:border-white dark:text-white rounded bg-transparent mr-2 bg-transparent"
+				placeholder="Ask the Genie"
+				on:keydown={(e) => e.key === 'Enter' && sendMessage()}
+			/>
+			<Button on:click={sendMessage}>
+				<ArrowLeftFromLine />
+			</Button>
+		</div>
 	</div>
 </main>
 
